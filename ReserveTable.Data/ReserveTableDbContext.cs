cfa2af0ae57add262827d6b2dd22f4d646343713
Mihaela Karadocheva <mkaradocheva6@gmail.com>
2019-07-01 21:57:25 +0300

    Added project with DbContext

---
+++ ReserveTable.Data/ReserveTableDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ReserveTable.Data
+{
+    public class ReserveTableDbContext : IdentityDbContext
+    {
+        public ReserveTableDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
