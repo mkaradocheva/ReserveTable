6b7c2e3b0e06a46f1c436a144d0b2f2085d53bcd
Mihaela Karadocheva <mkaradocheva6@gmail.com>
2019-07-04 18:28:58 +0300

    Added Restaurant create

--- ReserveTable/Controllers/RestaurantsController.cs
+++ ReserveTable/Controllers/RestaurantsController.cs
@@ -1,6 +1,10 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ReserveTable.App.Models.Restaurants;
 using ReserveTable.Data;
+using ReserveTable.Domain;
 using ReserveTable.Services;
 
 namespace ReserveTable.App.Controllers
@@ -24,6 +28,28 @@ namespace ReserveTable.App.Controllers
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateRestaurantModelView modelView)
+        {
+            //TODO: Unique restaurants in database
+
+            string cityId = dbContext.Cities
+                .Where(c => c.Name == modelView.City)
+                .Select(c => c.Id)
+                .SingleOrDefault();
+
+                var restaurant = new Restaurant
+                {
+                    Name = modelView.Name,
+                    CityId = cityId,
+                };
+
+                await dbContext.Restaurants.AddAsync(restaurant);
+                await dbContext.SaveChangesAsync();
+
+                return this.Redirect("/Home/Index");
         }
     }
 }
